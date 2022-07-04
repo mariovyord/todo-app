f9e2201190f7a20f66c28f15f823a2d28ab12224
NecroBread <mariovyordanov@gmail.com>
2022-07-04 20:48:55 +0300

    Add marking as completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function Todo({ value, _id, deleteTask }) {
 
-	const handleClick = () => {
+	const [completed, setCompleted] = useState(false);
+
+	const handleComplete = () => {
+		setCompleted(!completed);
+	}
+
+	const handleDelete = () => {
 		deleteTask(_id);
 	}
 
 	return (
 		<div>
-			<p>{value} <RiDeleteBin6Line onClick={handleClick} /></p>
+			<p onClick={handleComplete} className={completed ? 'completed' : ''}>{value}
+				<RiDeleteBin6Line onClick={handleDelete} />
+			</p>
 		</div>
 	)
 }
